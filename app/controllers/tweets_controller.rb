a1e3c0bab66ac4e602c456b883b2d617ce7a0b31
kazu-oku1 <100656002+kazu-oku1@users.noreply.github.com>
2022-08-20 22:19:29 +0900

    非同期通信

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,63 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, except: [:index, :new, :create, :search]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
+  def index
+    @tweets = Tweet.includes(:user)
+    @tweets = Tweet.order(id: "DESC" )
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def search
+    @tweets = SearchTweetsService.search(params[:keyword])
+  end
+
+  def edit
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path(@tweet)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @tweet.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :material, :guide, :recipe, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @tweet.user
+  end
+end
